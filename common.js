0489052b90deed22ce9c9739de70e7efaf1c95db
Tomas Persson <hypotesfabriken@gmail.com>
2024-07-07 11:33:26 +0200

    Structure common.js

---
+++ common.js
@@ -0,0 +1,16 @@
+
+        // Load external HTML components
+        function loadHTML(id, url) {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById(id).innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", url, true);
+            xhttp.send();
+        }
+
+        loadHTML("menu", "menu.html");
+        loadHTML("header", "header.html");
+        loadHTML("footer", "footer.html");
